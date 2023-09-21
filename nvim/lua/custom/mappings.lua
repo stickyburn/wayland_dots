4e294d416235164c8edb53ad318f1770147a030c
Yashank <yashank@hey.com>
2023-09-21 17:39:08 +0530

    chore: nvim custom lsp

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -1,22 +1,10 @@
+---@type MappingsTable
 local M = {}
 
 M.general = {
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } }
-  }
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  },
 }
-
--- M.nvterm = {
---   plugin = true,
---   n = {
---     -- toggle in normal mode
---     ["<A-h>"] = {
---       function()
---         require("nvterm.terminal").toggle "float"
---       end,
---       "Toggle floating term",
---     },
---   }
--- }
 
 return M
---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,48 @@
+local overrides = require("custom.configs.overrides")
+
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Override plugin definition options
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+}
+
+return plugins
