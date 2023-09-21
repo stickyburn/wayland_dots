1f797082411f7906f8fbe74da0e3d922eccab8a9
Yashank <yashank@hey.com>
2023-09-21 17:43:53 +0530

    chore: custom nvim lsp

---
+++ nvim/lua/custom/configs/null-ls.lua
@@ -0,0 +1,13 @@
+local null_ls = require "null-ls"
+
+local b = null_ls.builtins
+
+local sources = {
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } },
+  b.formatting.stylua,
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+}
---
+++ nvim/lua/custom/configs/overrides.lua
@@ -0,0 +1,47 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "markdown",
+    "markdown_inline",
+    "go",
+  },
+  indent = {
+    enable = true,
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    "lua-language-server",
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "prettier",
+    "gopls",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
