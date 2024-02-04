f4a2ca958485125764fb8ee05840017333c40aeb
stickburn <yashank@hey.com>
2024-02-04 23:14:59 +0530

    style: sexier fonts

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,15 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
-config.font = wezterm.font_with_fallback {
-	'Liga Comic Shanns',
-}
+config.font = wezterm.font_with_fallback({
+	"Liga Comic Shanns",
+})
 config.font_size = 11.0
 config.colors = {
-	background = '#362E45',
-	foreground = '#FFCCDD'
+	background = "#362E45",
+	foreground = "#FFCCDD",
 }
 config.enable_tab_bar = false
-config.default_cursor_style = 'SteadyUnderline'
+config.default_cursor_style = "SteadyUnderline"
 
 return config
