e81919751683f3c1554fd9f81439278dce6264ec
stickburn <yashank@hey.com>
2024-02-10 02:37:48 +0530

    feat: bookmarks; terminal movements

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,17 +1,24 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
+
 local config = {}
 
+-- "#362E45",
 config.font = wezterm.font_with_fallback({
 	"Liga Comic Shanns",
-	"Symbols Nerd Font Mono",
+	"JoyPixels",
 })
 config.font_size = 10.0
 config.colors = {
-	background = "#362E45",
+	background = "#30293d",
 	foreground = "#FFCCDD",
 }
--- config.enable_tab_bar = false
 config.default_cursor_style = "SteadyUnderline"
 config.hide_tab_bar_if_only_one_tab = true
+
+config.keys = {
+	{ key = "PageUp", action = act.ScrollByLine(-1) },
+	{ key = "PageDown", action = act.ScrollByLine(1) },
+}
 
 return config
